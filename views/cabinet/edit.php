3096eab7fb81ffa275e64d6d271b7191553452b9
Igor Savelev <savelevi@mail.ru>
2020-02-21 03:14:13 +0300

    PHPdocs added.

---
+++ views/cabinet/edit.php
@@ -0,0 +1,62 @@
+<?php include ROOT . '/views/layouts/header.php'; ?>
+
+<section class="section-inner">
+    <div class="container">
+        <div class="row">
+            <!-- Page navigation -->
+            <?php echo $pagination->get(); ?>
+            <div class="col-sm-12">
+                <div class="features_items">
+                    <h4>Список задач (панель администратора)</h4>
+                    <br/>
+                    <table class="table-bordered table-striped table">
+                        <tr>
+                            <th><a href="/cabinet/order-id/page-1"><i class="fa fa-sort"></i>ID задачи</a></th>
+                            <th><a href="/cabinet/order-name/page-1"><i class="fa fa-sort"></i>Имя</a></th>
+                            <th><a href="/cabinet/order-email/page-1"><i class="fa fa-sort"></i>Email</a></th>
+                            <th>Описание</th>
+                            <th><a href="/cabinet/order-status/page-1"><i class="fa fa-sort"></i>Статус</a></th>
+                            <th>Категория</th>
+                            <th>Редактировать</th>
+                        </tr>
+                        <?php foreach ($tasksList as $task): ?>
+                            <tr>
+                                <td><?php echo $task['id']; ?></td>
+                                <td><?php echo htmlentities($task['name']); ?></td>
+                                <td><?php echo htmlentities($task['email']); ?></td>
+                                <td><?php echo htmlentities($task['text']); ?></td>  
+                                <td><?php echo $task['status']; ?></td> 
+                                <td><?php echo $task['category']; ?></td>  
+                                <td><a href="/cabinet/update/<?php echo $task['id']; ?>" title="Редактировать"><i class="fa fa-pencil-square-o"></i></a></td>  
+                            </tr>
+                        <?php endforeach; ?>
+                    </table>
+                </div>
+                <div class="col-sm-4 col-sm-offset-4 padding-right">
+                    <?php if (isset($errors) && is_array($errors)): ?>
+                        <ul>
+                            <?php foreach ($errors as $error): ?>
+                                <li class="error"> - <?php echo $error; ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endif; ?>
+                    <div class="signup-form"><!--sign up form-->
+                        <h2>Новая задача</h2>
+                        <br/>
+                        <form action="/cabinet/create" method="post">
+                            <p>Имя (только буквы и пробелы):</p><input type="text" name="name" placeholder="Name" required/>
+                            <p>Email:</p><input type="email" name="email" placeholder="E-mail" required/>
+                            <p>Описание задачи (не менее 3 символов):</p><textarea  name="text" required></textarea>
+                            <br/><br/>
+                            <input type="submit" name="submit" class="btn btn-default" value="Отправить" />
+                        </form>
+                    </div><!--/sign up form-->
+                    <br/>
+                    <br/>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include ROOT . '/views/layouts/footer.php'; ?>
---
+++ views/cabinet/index.php
@@ -0,0 +1,16 @@
+<?php include ROOT . '/views/layouts/header.php'; ?>
+
+<section class="section-inner">
+    <div class="container">
+        <div class="row">
+            <h3>Кабинет пользователя</h3>
+            <h4>Добрый день, администратор!</h4>
+            <p>Вам доступны следующие действия:</p>
+            <ul>
+                <li><a href="/cabinet/order-id/page-1">Редактирование задач</a></li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+<?php include ROOT . '/views/layouts/footer.php'; ?>
